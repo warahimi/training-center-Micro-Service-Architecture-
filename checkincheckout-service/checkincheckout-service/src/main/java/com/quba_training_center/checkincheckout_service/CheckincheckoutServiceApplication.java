f10988df04e8243a3f138c96a2ab76ec5cdf7de5
Wahidullah Rahimi <wahidrahimi45@gmail>
2024-08-28 17:51:56 -0700

    Initail Setup

---
+++ checkincheckout-service/checkincheckout-service/src/main/java/com/quba_training_center/checkincheckout_service/CheckincheckoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quba_training_center.checkincheckout_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckincheckoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckincheckoutServiceApplication.class, args);
+	}
+
+}
---
+++ student-service/student-service/src/test/java/com/quba_training_center/student_service/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quba_training_center.student_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
